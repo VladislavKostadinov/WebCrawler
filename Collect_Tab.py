a5b17f8c538be7bdb0e784569c712cbefba9f9fa
VladislavKostadinov <79514275+VladislavKostadinov@users.noreply.github.com>
2022-03-03 20:09:09 +0200

    Add files via upload

---
+++ Collect_Tab.py
@@ -0,0 +1,182 @@
+import re
+import sys
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtWidgets import QWidget, QApplication, QTabWidget, QLabel, QHBoxLayout, QButtonGroup, QVBoxLayout, \
+    QRadioButton, QGroupBox, QPushButton, QGridLayout, QMessageBox, QCheckBox, QMainWindow, QFrame, QLineEdit
+import time
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import WebCrawler
+import Main_Tab
+import Browse_Tab
+
+actions_f = []
+beginning = []
+forward_btn = []
+actions_s = []
+scrape_btn = []
+counter = []
+collectibles = []
+book_info = ['book_title', 'book_author', 'book_pages', 'book_price']
+book_info = ['book_title', 'book_author', 'book_pages', 'book_price']
+book_info_df_main = pd.DataFrame(None, columns=book_info)
+
+def start_process(self):
+    actions_f.append('Action')
+    beginning.append('Start')
+    if self.display_state.text() == '7/7':
+        QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on', QMessageBox.Ok,
+                                QMessageBox.Ok)
+        return
+    if len(forward_btn) > 0:
+        QMessageBox.information(self, 'Scrape Box', 'Process already started.', QMessageBox.Ok,
+                                QMessageBox.Ok)
+        return Main_Tab.books_sample[0][0]
+    else:
+        forward_btn.append(Main_Tab.books_sample)
+        print(len(Main_Tab.books_sample))
+        print(actions_s)
+        print(actions_f)
+        return Main_Tab.books_sample[0][0]
+
+
+def forward_click(self):
+
+    if self.display_state.text() == '7/7':
+        QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on', QMessageBox.Ok,
+                                QMessageBox.Ok)
+
+        return
+
+    if len(beginning) == 0:
+
+        QMessageBox.information(self, 'Scrape Box', 'Please start the process first', QMessageBox.Ok,
+                                QMessageBox.Ok)
+        return
+        # actions_f.pop(0)
+    else:
+        if len(actions_f) <= len(actions_s):
+            actions_f.append('Action')
+            try:
+                forward_btn[0][0].pop(0)
+            except IndexError:
+                QMessageBox.information(self, 'Scrape Box', 'An error occured!', QMessageBox.Ok,
+                                        QMessageBox.Ok)
+                return
+            if len(forward_btn[0]) == 0:
+                print("End")
+                QMessageBox.information(self, 'Scrape Box', 'No more pages to load', QMessageBox.Ok,
+                                        QMessageBox.Ok)
+                return
+            print(actions_f)
+            return forward_btn[0][0][0]
+        else:
+            QMessageBox.information(self, 'Scraper window', "Scrape page first!", QMessageBox.Ok,
+                                    QMessageBox.Ok)
+            print(forward_btn[0][0][0])
+            return forward_btn[0][0][0]
+
+
+def scrape_segments(self):
+    mismatch = 0
+
+    if len(beginning) == 0:
+        QMessageBox.information(self, 'Scrape Box', 'Start process first', QMessageBox.Ok,
+                                QMessageBox.Ok)
+        # actions_s.pop(0)
+        return
+    else:
+        if self.display_state.text() == '7/7':
+            QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on', QMessageBox.Ok,
+                                    QMessageBox.Ok)
+            return
+        else:
+            if len(actions_f) > len(actions_s):
+                actions_s.append('Scrape')
+
+                title, author, pages, price = [], [], [], []
+                scrape_btn.append(Main_Tab.books_sample[0])
+                browser = webdriver.Chrome(WebCrawler.web_driver)
+                browser.maximize_window()
+                browser.implicitly_wait(10)
+                browser.get(scrape_btn[0][0])
+                time.sleep(3)
+                scrape_btn[0].pop(0)
+                page_body = browser.find_element(By.TAG_NAME, 'body')
+                scroll_downs = 2
+                while scroll_downs:
+                    page_body.send_keys(Keys.PAGE_DOWN)
+                    time.sleep(1)
+                    scroll_downs -= 1
+
+                find_tag = browser.find_element(By.CLASS_NAME, 'item-info h1')
+                title.append(find_tag.text)
+
+                find_tag = browser.find_element(By.CLASS_NAME, 'author-info span a span')
+                author.append(find_tag.text)
+
+                find_tag = browser.find_element(By.CLASS_NAME, 'biblio-info li span span')
+                pages.append(find_tag.text)
+
+                find_tag = browser.find_element(By.CLASS_NAME, 'sale-price')
+                priced = find_tag.text[3:]
+                price.append(priced)
+
+                book_profile = {
+                    'book_title': title,
+                    'book_author': author,
+                    'book_pages': pages,
+                    'book_price': price
+                }
+
+                try:
+                    add_new_sample = pd.DataFrame(book_profile, columns=book_info)
+                    self.book_info_df = self.book_info_df.append(add_new_sample, ignore_index=True)
+                    time.sleep(1)
+                except:
+                    mismatch += 1
+                    print(mismatch)
+                    pass
+                browser.quit()
+                counter.append('Object')
+                collectibles.append('Item')
+                collectibles.append('Item')
+                collectibles.append('Item')
+                collectibles.append('Item')
+                self.display_state.setText(str(len(counter)) + '/7')
+                self.coll_.setText(str(len(collectibles)))
+                self.totl_.setText(str(len(collectibles)))
+                if self.display_state.text() == '7/7':
+                    self.display_state.setObjectName('complete')
+                    self.craw_mess.setObjectName('tez')
+                    self.display_state.style().unpolish(self.display_state)
+                    self.craw_mess.style().unpolish(self.craw_mess)
+                    self.craw_mess.setText('Finished')
+                    QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on.', QMessageBox.Ok,
+                                            QMessageBox.Ok)
+                    return
+                print(actions_s)
+                return self.book_info_df, mismatch
+            else:
+                QMessageBox.information(self, 'Scrape Box', 'Please load next page first!', QMessageBox.Ok,
+                                        QMessageBox.Ok)
+                return self.book_info_df, mismatch
+
+
+def save_file(self):
+    try:
+        if not self.display_state.text() == '0/7':
+            self.book_info_df.to_csv(r'files/books.csv')
+            QMessageBox.information(self, 'Depository Window', 'Your data has been saved.', QMessageBox.Ok,
+                                    QMessageBox.Ok)
+        else:
+            QMessageBox.information(self, 'Depository Window', 'No collected data yet.', QMessageBox.Ok,
+                                    QMessageBox.Ok)
+    except:
+        QMessageBox.information(self, 'Depository Window', 'An error occurred!', QMessageBox.Ok,
+                                QMessageBox.Ok)
--- WebCrawler.py
+++ WebCrawler.py
@@ -11,7 +11,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
-
+import Main_Tab
+import Collect_Tab
+import Browse_Tab
 web_driver = r'../chromedriver/chromedriver.exe'
 
 style_sheets = """ 
@@ -71,17 +73,10 @@ style_sheets = """
    QWidget#search_body {
    background-color: beige; }
 """
-book_urls = []
 second_book_urls = []
-books_sample = []
-forward_btn = []
-scrape_btn = []
-counter = []
-collectibles = []
-actions_f = []
-actions_s = []
-main_page_c = []
-beginning = []
+
+
+book_info = ['book_title', 'book_author', 'book_pages', 'book_price']
 book_info = ['book_title', 'book_author', 'book_pages', 'book_price']
 book_info_df_main = pd.DataFrame(None, columns=book_info)
 
@@ -162,23 +157,15 @@ class MainScraper(QWidget):
 
         self.load = QPushButton("Load website")
         self.scrape = QPushButton("Scrape website")
-        self.scrape.clicked.connect(self.scrape_url)
-        self.load.clicked.connect(lambda: web_view.load(QUrl(self.load_main())))
+        self.scrape.clicked.connect(lambda: Main_Tab.scrape_url(self))
+        # self.llink = Main_Tab.load_main(self)
+        self.load.clicked.connect(lambda: web_view.load(QUrl(Main_Tab.load_main(self))))
         main_wind = QVBoxLayout(self)
         main_wind.addWidget(self.status)
         main_wind.addWidget(self.load)
         main_wind.addWidget(web_view, Qt.AlignCenter)
         main_wind.addWidget(self.scrape)
         self.main_scrape.setLayout(main_wind)
-
-    def load_main(self):
-        if len(main_page_c) == 0:
-            main_page_c.append('Main')
-            return 'https://www.bookdepository.com/category/355/Fantasy/browse/viewmode/all'
-        else:
-            QMessageBox.information(self, 'Main Window', 'Initial site already loaded. Please scrape.',
-                                    QMessageBox.Ok, QMessageBox.Ok)
-            return 'https://www.bookdepository.com/category/355/Fantasy/browse/viewmode/all'
 
     def load_img(self, path):
         try:
@@ -239,10 +226,10 @@ class MainScraper(QWidget):
         self.next_page = QPushButton("Load next page")
         self.scraper = QPushButton("Scrape page")
         self.save_to = QPushButton("Save to file")
-        self.scraper.clicked.connect(self.scrape_segments)
-        self.loader.clicked.connect(lambda: web_v_.load(QUrl(self.start_process())))
-        self.next_page.clicked.connect(lambda: web_v_.load(QUrl(self.forward_click())))
-        self.save_to.clicked.connect(self.save_file)
+        self.scraper.clicked.connect(lambda: Collect_Tab.scrape_segments(self))
+        self.loader.clicked.connect(lambda: web_v_.load(QUrl(Collect_Tab.start_process(self))))
+        self.next_page.clicked.connect(lambda: web_v_.load(QUrl(Collect_Tab.forward_click(self))))
+        self.save_to.clicked.connect(lambda: Collect_Tab.save_file(self))
 
         buttons = QWidget()
         processes = QHBoxLayout(self)
@@ -302,7 +289,7 @@ class MainScraper(QWidget):
         self.book_name = QLineEdit(self)
         self.book_title.setFont(QFont("Verdana", 15))
         self.search_eng = QPushButton("Search")
-        self.search_eng.clicked.connect(self.searching)
+        self.search_eng.clicked.connect(lambda: Browse_Tab.searching(self))
 
         e_kets = QWidget()
         book_ttl = QLabel("Title: ")
@@ -329,7 +316,6 @@ class MainScraper(QWidget):
         self.book_pages_e = QLabel("")
         self.book_pages_e.setFont(QFont("Verdana", 7))
         self.book_pages_e.setObjectName('entry')
-
 
         greet = QWidget()
         grettings = QHBoxLayout()
@@ -412,264 +398,16 @@ class MainScraper(QWidget):
         complete_all.addWidget(ind)
         complete_all.addWidget(body)
 
-
         self.catalogue.setLayout(complete_all)
 
-    def scrape_url(self):
-        p = 1
-        i = 14
-        iz = 14
-        if len(books_sample) > 0:
-            QMessageBox.information(self, 'Main Window', 'Initial website already scraped! Please continue.',
-                                    QMessageBox.Ok, QMessageBox.Ok)
-            return books_sample
-        if len(main_page_c) == 1:
-
-            browser = webdriver.Chrome(web_driver)
-            browser.maximize_window()
-            browser.implicitly_wait(20)
-
-            browser.get('https://www.bookdepository.com/category/355/Fantasy/browse/viewmode/all?=page=' + str(p))
-            time.sleep(3)
-            page_body = browser.find_element(By.TAG_NAME, "body")
-
-            scroll_downs = 2
-
-            while scroll_downs:
-                page_body.send_keys(Keys.PAGE_DOWN)
-                time.sleep(1)
-                scroll_downs -= 1
-            time.sleep(2)
-            p += 1
-
-            b_tag = browser.find_elements(By.CLASS_NAME, "item-img a")
-
-            for a in b_tag:
-                book_urls.append(a.get_attribute("href"))
-            books_sample.append(book_urls[:i])
-            self.mandatory_s()
-            btn = browser.find_element(By.XPATH, '//a[text()="' + str(p) + '"]')
-            if not scroll_downs:
-                btn.click()
-                time.sleep(2)
-
-            while p < 3:
-                p += 1
-                iz += 14
-                scroll_downs = 2
-                page_body = browser.find_element(By.TAG_NAME, "body")
-
-                while scroll_downs:
-                    page_body.send_keys(Keys.PAGE_DOWN)
-                    time.sleep(1)
-                    scroll_downs -= 1
-
-                bs_tag = browser.find_elements(By.CLASS_NAME, "item-img a")
-                for a in bs_tag:
-                    book_urls.append(a.get_attribute("href"))
-                for x in book_urls[i:iz]:
-                    books_sample[0].append(x)
-                self.mandatory_s()
-                btn = browser.find_element(By.XPATH, '//a[text()="' + str(p) + '"]')
-
-                if not scroll_downs and p < 4:
-                    i += 14
-                    iz += 14
-                    btn.click()
-                    time.sleep(2)
-
-            browser.quit()
-            QMessageBox.information(self, 'Main Window', 'Initial website scraped! Please continue.', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-            return books_sample
-        else:
-            QMessageBox.information(self, 'Main Window', 'Please load the initial site!', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-            return
-
     def mandatory_s(self):
-        self.craw_act.setText('Ready')
+        self.craw_act.setText('Re   ady')
         self.display_status.setText('Data acquired')
         self.craw_act.setObjectName('tez')
         self.display_status.setObjectName('green')
         self.craw_act.style().unpolish(self.craw_act)
         self.display_status.style().unpolish(self.display_status)
-        self.collection.setText(str(len(books_sample[0])))
-
-    def start_process(self):
-        actions_f.append('Action')
-        beginning.append('Start')
-        if self.display_state.text() == '7/7':
-            QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-            return
-        if len(forward_btn) > 0:
-            QMessageBox.information(self, 'Scrape Box', 'Process already started.', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-            return books_sample[0][0]
-        else:
-            forward_btn.append(books_sample[0])
-            print(len(books_sample[0]))
-
-            return books_sample[0][0]
-
-    def forward_click(self):
-
-        if self.display_state.text() == '7/7':
-            QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-
-            return
-
-        if len(beginning) == 0:
-
-            QMessageBox.information(self, 'Scrape Box', 'Please start the process first', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-            return
-            # actions_f.pop(0)
-        else:
-            if len(actions_f) <= len(actions_s):
-                actions_f.append('Action')
-                try:
-                    forward_btn[0].pop(0)
-                except IndexError:
-                    QMessageBox.information(self, 'Scrape Box', 'An error occured!', QMessageBox.Ok,
-                                            QMessageBox.Ok)
-                    return
-                if len(forward_btn[0]) == 0:
-                    print("End")
-                    QMessageBox.information(self, 'Scrape Box', 'No more pages to load', QMessageBox.Ok,
-                                            QMessageBox.Ok)
-                    return
-                return forward_btn[0][0]
-            else:
-                QMessageBox.information(self, 'Scraper window', "Scrape page first!", QMessageBox.Ok,
-                                        QMessageBox.Ok)
-                return forward_btn[0][0]
-
-    def scrape_segments(self):
-        mismatch = 0
-
-        if len(beginning) == 0:
-            QMessageBox.information(self, 'Scrape Box', 'Start process first', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-            # actions_s.pop(0)
-            return
-        else:
-            if self.display_state.text() == '7/7':
-                QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on', QMessageBox.Ok,
-                                        QMessageBox.Ok)
-                return
-            else:
-                if len(actions_f) > len(actions_s):
-                    actions_s.append('Scrape')
-
-                    title, author, pages, price = [], [], [], []
-                    scrape_btn.append(books_sample[0])
-                    browser = webdriver.Chrome(web_driver)
-                    browser.maximize_window()
-                    browser.implicitly_wait(10)
-                    browser.get(scrape_btn[0][0])
-                    time.sleep(3)
-                    scrape_btn[0].pop(0)
-                    page_body = browser.find_element(By.TAG_NAME, 'body')
-                    scroll_downs = 2
-                    while scroll_downs:
-                        page_body.send_keys(Keys.PAGE_DOWN)
-                        time.sleep(1)
-                        scroll_downs -= 1
-
-                    find_tag = browser.find_element(By.CLASS_NAME, 'item-info h1')
-                    title.append(find_tag.text)
-
-                    find_tag = browser.find_element(By.CLASS_NAME, 'author-info span a span')
-                    author.append(find_tag.text)
-
-                    find_tag = browser.find_element(By.CLASS_NAME, 'biblio-info li span span')
-                    pages.append(find_tag.text)
-
-                    find_tag = browser.find_element(By.CLASS_NAME, 'sale-price')
-                    priced = find_tag.text[3:]
-                    price.append(priced)
-
-                    book_profile = {
-                        'book_title': title,
-                        'book_author': author,
-                        'book_pages': pages,
-                        'book_price': price
-                    }
-
-                    try:
-                        add_new_sample = pd.DataFrame(book_profile, columns=book_info)
-                        self.book_info_df = self.book_info_df.append(add_new_sample, ignore_index=True)
-                        time.sleep(1)
-                    except:
-                        mismatch += 1
-                        print(mismatch)
-                        pass
-                    browser.quit()
-                    counter.append('Object')
-                    collectibles.append('Item')
-                    collectibles.append('Item')
-                    collectibles.append('Item')
-                    collectibles.append('Item')
-                    self.display_state.setText(str(len(counter)) + '/7')
-                    self.coll_.setText(str(len(collectibles)))
-                    self.totl_.setText(str(len(collectibles)))
-                    if self.display_state.text() == '7/7':
-                        self.display_state.setObjectName('complete')
-                        self.craw_mess.setObjectName('tez')
-                        self.display_state.style().unpolish(self.display_state)
-                        self.craw_mess.style().unpolish(self.craw_mess)
-                        self.craw_mess.setText('Finished')
-                        QMessageBox.information(self, 'Scrape Box', 'Search finished, please carry on.', QMessageBox.Ok,
-                                                QMessageBox.Ok)
-                        return
-                    return self.book_info_df, mismatch
-                else:
-                    QMessageBox.information(self, 'Scrape Box', 'Please load next page first!', QMessageBox.Ok,
-                                            QMessageBox.Ok)
-                    return self.book_info_df, mismatch
-
-    def save_file(self):
-        try:
-            if not self.display_state.text() == '0/7':
-                self.book_info_df.to_csv(r'files/books.csv')
-                QMessageBox.information(self, 'Depository Window', 'Your data has been saved.', QMessageBox.Ok,
-                                        QMessageBox.Ok)
-            else:
-                QMessageBox.information(self, 'Depository Window', 'No collected data yet.', QMessageBox.Ok,
-                                        QMessageBox.Ok)
-        except:
-            QMessageBox.information(self, 'Depository Window', 'An error occurred!', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-
-    def searching(self):
-        if self.book_name.text() == '':
-            QMessageBox.information(self, 'Search Box', 'Please enter a value first.', QMessageBox.Ok,
-                                    QMessageBox.Ok)
-        else:
-            try:
-
-                auth = self.book_info_df.loc[self.book_info_df['book_title'].str.lower() ==
-                                             self.book_name.text().lower(), 'book_author'].item()
-                self.book_auth_e.setText(str(auth))
-                ttl = self.book_info_df.loc[self.book_info_df['book_title'].str.lower() ==
-                                            self.book_name.text().lower(), 'book_title'].item()
-                self.book_ttl_e.setText(str(ttl))
-                pages = self.book_info_df.loc[self.book_info_df['book_title'].str.lower() ==
-                                              self.book_name.text().lower(),
-                                              'book_pages'].item()
-                self.book_pages_e.setText(str(pages))
-                price = self.book_info_df.loc[self.book_info_df['book_title'].str.lower() ==
-                                              self.book_name.text().lower(),
-                                              'book_price'].item()
-                self.book_price_e.setText(str(price))
-            except:
-                QMessageBox.information(self, 'Search Box', 'Invalid title. Try again', QMessageBox.Ok,
-                                        QMessageBox.Ok)
-                return
-
+        self.collection.setText(str(len(Main_Tab.books_sample[0])))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
